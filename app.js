26c133f93f034785f8fb32ce3a8c0434de190358
Emerson Ferreira <emerson.efs2@hotmail.com>
2025-01-30 17:09:28 -0300

    Criar lista amigos, Adiciona função para adicionar amigos na lista

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = []
+
+
+function adicionarAmigo() {
+    let campo = document.querySelector('input').value;
+
+    if (campo == "") {
+        alert("Por favor, insira um nome.");
+    } else {
+        amigos.push(campo);
+        limparCampo();
+        console.log(amigos);
+    }
+
+    function limparCampo() {
+        campo = document.querySelector('input');
+        campo.value = '';
+    }
+
+}
